ad9f3991a623f8c109c063397bd80db344cbb916
Matt F <matthewf662@gmail.com>
2013-03-10 23:26:51 -0400

    Added setDelete
    same as setAdd except decrese the quantity.

--- setItem_Add_Delete/ConsoleApplication1/setItemAddDelete.cpp
+++ setItem_Add_Delete/ConsoleApplication1/setItemAddDelete.cpp
@@ -3,15 +3,6 @@
 #include <fstream>
 
 using namespace std;
-void Print(string code, string description, long cost, int quantity)
-{
-     cout << code;
-     cout << description;
-     cout << cost;
-     cout << quantity << endl;
-     
-     return;
-}
 
  void setItemData (string code, string description, long cost, int quantity){
       
@@ -58,8 +49,7 @@ void Print(string code, string description, long cost, int quantity)
 	ifstream inputFile; 
 	ofstream outputFile;
 	string currentLine;
-	string tempCode, Description,trash;
-	string previousCode;
+	string tempCode, Description;
 	long cost;
 	int quantity;
 	inputFile.open("input.txt");//opens input file
@@ -73,13 +63,13 @@ void Print(string code, string description, long cost, int quantity)
 	}
 	if (!outputFile)
 	{
-		cerr<<"Can't open input file\n";
+		cerr<<"Can't open output file\n";
 		system("PAUSE");
 		exit(1);
 	}
 	while(!inputFile.eof()){
 	inputFile >> tempCode;
-		if(!(tempCode=="\n")){//&&tempCode != previousCode
+		if(!(tempCode=="\n")){
 			inputFile>> Description >> cost >> quantity;
 			if(Description!=""){
 				outputFile << tempCode <<"\t" << Description << "\t" << cost << "\t";
@@ -89,7 +79,50 @@ void Print(string code, string description, long cost, int quantity)
 			outputFile << quantity << "\n";
 			}
 		}
-	previousCode=tempCode;
+	Description = "";
+	}
+
+    inputFile.close();
+	outputFile.close();
+	remove("input.txt");
+	rename("output.txt","input.txt");
+ }
+
+  void setDeleteQuantity(string code, int decrement){
+      
+	ifstream inputFile; 
+	ofstream outputFile;
+	string currentLine;
+	string tempCode, Description;
+	long cost;
+	int quantity;
+	inputFile.open("input.txt");//opens input file
+	outputFile.open("output.txt");//opens the output file
+
+	if (!inputFile)
+	{
+		cerr<<"Can't open input file\n";
+		system("PAUSE");
+		exit(1);
+	}
+	if (!outputFile)
+	{
+		cerr<<"Can't open output file\n";
+		system("PAUSE");
+		exit(1);
+	}
+	while(!inputFile.eof()){
+	inputFile >> tempCode;
+		if(!(tempCode=="\n")){
+			inputFile>> Description >> cost >> quantity;
+			if(Description!=""){
+				outputFile << tempCode <<"\t" << Description << "\t" << cost << "\t";
+				if(tempCode==code){
+						quantity -= decrement;
+				}
+			outputFile << quantity << "\n";
+			}
+		}
 	Description = "";
 	}
 
@@ -101,8 +134,13 @@ void Print(string code, string description, long cost, int quantity)
 
 int main(int argc, char *argv[])
 {
-	//setItemData("code9","des69", 696, 69);
+	setItemData("code1", "des1", 123, 1);
+	setItemData("code2", "des2", 456, 2);
+	setItemData("code3", "des3", 789, 3);
+	setItemData("code4", "des4", 101, 4);
 	setAddQuantity("code2", 20);
+	setDeleteQuantity("code4", 5);
+	setAddQuantity("code4", 4);
 	system("PAUSE");
 	return EXIT_SUCCESS;
 }
