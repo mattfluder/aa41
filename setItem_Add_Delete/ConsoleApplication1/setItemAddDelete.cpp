986eab34a211fa61104e0e755a0dce51eba564af
Matt F <matthewf662@gmail.com>
2013-03-10 23:03:48 -0400

    added setAdd

--- setItem_Add_Delete/ConsoleApplication1/setItemAddDelete.cpp
+++ setItem_Add_Delete/ConsoleApplication1/setItemAddDelete.cpp
@@ -47,7 +47,52 @@ void Print(string code, string description, long cost, int quantity)
 	if (!spotFound){
 		outputFile << code <<"\t" << description << "\t" << cost << "\t" << quantity << "\n";
 	}
-	//Print(code, description, cost, quantity);
+    inputFile.close();
+	outputFile.close();
+	remove("input.txt");
+	rename("output.txt","input.txt");
+ }
+
+ void setAddQuantity(string code, int increment){
+      
+	ifstream inputFile; 
+	ofstream outputFile;
+	string currentLine;
+	string tempCode, Description,trash;
+	string previousCode;
+	long cost;
+	int quantity;
+	inputFile.open("input.txt");//opens input file
+	outputFile.open("output.txt");//opens the output file
+
+	if (!inputFile)
+	{
+		cerr<<"Can't open input file\n";
+		system("PAUSE");
+		exit(1);
+	}
+	if (!outputFile)
+	{
+		cerr<<"Can't open input file\n";
+		system("PAUSE");
+		exit(1);
+	}
+	while(!inputFile.eof()){
+	inputFile >> tempCode;
+		if(!(tempCode=="\n")){//&&tempCode != previousCode
+			inputFile>> Description >> cost >> quantity;
+			if(Description!=""){
+				outputFile << tempCode <<"\t" << Description << "\t" << cost << "\t";
+				if(tempCode==code){
+					quantity += increment;
+				}
+			outputFile << quantity << "\n";
+			}
+		}
+	previousCode=tempCode;
+	Description = "";
+	}
+
     inputFile.close();
 	outputFile.close();
 	remove("input.txt");
@@ -56,7 +101,8 @@ void Print(string code, string description, long cost, int quantity)
 
 int main(int argc, char *argv[])
 {
-	setItemData("code9","des69", 696, 69);
+	//setItemData("code9","des69", 696, 69);
+	setAddQuantity("code2", 20);
 	system("PAUSE");
 	return EXIT_SUCCESS;
 }
